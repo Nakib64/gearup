969d966b22634ce25b474b97aafe37de262920e8
Nakib <nafizuddowla01@gmail.com>
2026-07-08 23:43:24 +0600

    chore: clean up old users placeholder endpoints in app.ts

--- src/app.ts
+++ src/app.ts
@@ -16,16 +16,6 @@ app.get('/', (_req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.get('/users', async (_req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
-
-app.post('/users', async (req: Request, res: Response) => {
-  const { email, name } = req.body;
-  const user = await prisma.user.create({ data: { email, name } });
-  res.status(201).json(user);
-});
 
 const port = Number(process.env.PORT ?? 3000);
 app.listen(port, () => {
