b9f1261715fc84089c74bb000398b3fa0bd0603e
Nakib <nafizuddowla01@gmail.com>
2026-07-09 18:16:50 +0600

    swagger UI wasn't loading on vercel, so we have updated the swagger laoding through cdn

--- src/app.ts
+++ src/app.ts
@@ -75,7 +75,7 @@ const port = Number(process.env.PORT ?? 3000);
 if (!process.env.VERCEL) {
   app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
-  });
+  }); 
 }
 
 export default app;
